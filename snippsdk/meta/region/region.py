d19b622e57426fc2917deeb70eb16aaae8428a80
odaniel <danielmenezes0822@gmail.com>
2026-08-14 23:44:59 -0400

    doc: Documented Region()'s attributes

--- snippsdk/meta/region/region.py
+++ snippsdk/meta/region/region.py
@@ -4,5 +4,14 @@ class Region():
     """
     def __init__(self, json: dict) -> None:
         self.region: str = json["region"]
+        """
+        The region of it's region
+        """
         self.country: str = json["country"]
+        """
+        The country of it's region
+        """
         self.label: str = json["label"]
+        """
+        The label of it's region
+        """
