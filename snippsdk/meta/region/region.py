40f2d968ceeb73f4be0cc8121ba453ddb40ce06b
odaniel <danielmenezes0822@gmail.com>
2026-08-14 23:43:06 -0400

    docs: Documented Region

--- snippsdk/meta/region/region.py
+++ snippsdk/meta/region/region.py
@@ -1,4 +1,7 @@
 class Region():
+    """
+    Represents a region which snipp can route users to or be routed.
+    """
     def __init__(self, json: dict) -> None:
         self.region: str = json["region"]
         self.country: str = json["country"]
